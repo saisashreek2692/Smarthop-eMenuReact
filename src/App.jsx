d11c46855df53f89bd0ce4eb8b763efd949d85ee
Sai Sashreek <saisashreekd@gmail.com>
2024-04-30 14:19:29 +0530

    App Component Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
 
 const App = () => {
   return (
-    <div>
-      <h1>eMenu - Food Ordering App</h1>
-      <p>This app reduces carbon footprint</p>
+    <div className='app'>
+      <Navbar />
     </div>
   )
 }
