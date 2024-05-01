3829be3d9d8891ae4dc36e2b5b83910a6280a3f7
Sai Sashreek <saisashreekd@gmail.com>
2024-05-01 15:30:38 +0530

    App JS Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import Navbar from './Components/Navbar/Navbar'
+import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Cart from "./Pages/Cart/Cart";
+import Order from "./Pages/PlaceOrder/PlaceOrder";
+import Footer from "./Components/Footer/Footer";
 
 const App = () => {
   return (
-    <div className='app'>
-      <Navbar />
-    </div>
-  )
-}
+    <>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<Order />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-export default App
+export default App;
