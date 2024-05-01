e43bd9da75b82d65e02b0510857b2bec3846e425
Sai Sashreek <saisashreekd@gmail.com>
2024-05-01 15:31:55 +0530

    Header Section Added

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order Your Favourite Food Here</h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis,
+          architecto, debitis optio alias ab sit dicta tempore tenetur nesciunt
+          natus eos ipsa obcaecati esse provident reprehenderit sunt
+          voluptatibus veniam doloribus rem ratione. Eum, aperiam in!
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
