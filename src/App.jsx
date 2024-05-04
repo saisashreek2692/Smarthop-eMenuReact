37b7ea7a8a7a28138f98595fcf5ca7171a0ab36d
Sai Sashreek <saisashreekd@gmail.com>
2024-05-04 10:29:16 +0530

    Login Component Added to App Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import Cart from "./Pages/Cart/Cart";
 import Order from "./Pages/PlaceOrder/PlaceOrder";
 import Footer from "./Components/Footer/Footer";
+import LoginPopup from "./Components/LoginPopup/LoginPopup";
 
 const App = () => {
+
+  const [showLogin, setShowLogin] = useState(false);
+
   return (
     <>
+      {showLogin ? <LoginPopup /> : <></>}
       <div className="app">
-        <Navbar />
+        <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
