befaaf56f19fcc662d789d5d250f3ca2d3cf059c
Sai Sashreek <saisashreekd@gmail.com>
2024-05-04 15:31:04 +0530

    Basket Class Updated Dynamically

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import {assets} from "../../assets/assets";
 import { Link } from "react-router-dom";
+import { StoreContext } from "../../Context/StoreContext";
 
 const Navbar = ({setShowLogin}) => {
 
     const [menu, setMenu] = useState("home");
+    const {  getTotalCartAmount } = useContext(StoreContext);
 
   return (
     <div className='navbar'>
@@ -20,7 +22,7 @@ const Navbar = ({setShowLogin}) => {
             <img src={assets.search_icon} alt="" />
             <div className="navbar-search-icon">
                 <Link to="/cart"><img src={assets.basket_icon} alt="" /></Link>
-                <div className="dot"></div>
+                <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
             </div>
             <button onClick={() => setShowLogin(true)}>sign in</button>
         </div>
