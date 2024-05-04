9725ca9d86792b8ce3858ca4e1be301502a4998b
Sai Sashreek <saisashreekd@gmail.com>
2024-05-04 11:19:01 +0530

    Login UI Added with Funtionality

--- src/Components/LoginPopup/LoginPopup.jsx
+++ src/Components/LoginPopup/LoginPopup.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from "react";
+import { assets } from "../../assets/assets";
 
-const LoginPopup = () => {
+const LoginPopup = ({ setShowLogin }) => {
+  const [currentState, setCurrentState] = useState("Login");
+
   return (
-    <div>LoginPopup</div>
-  )
-}
+    <div className="login-popup">
+      <form className="login-popup-container">
+        <div className="login-popup-title">
+          <h2>{currentState}</h2>
+          <img
+            onClick={() => setShowLogin(false)}
+            src={assets.cross_icon}
+            alt="cross icon"
+          />
+        </div>
+        <div className="login-popup-inputs">
+          {currentState === "Login" ? (
+            <></>
+          ) : (
+            <input type="text" placeholder="Enter Your Name" required />
+          )}
+          <input type="email" placeholder="Enter Email" required />
+          <input type="password" placeholder="Enter Strong Password" required />
+        </div>
+        <button>
+          {currentState === "Sign Up" ? "Create Account" : "Login"}
+        </button>
+        <div className="login-popup-condition">
+          <input type="checkbox" required />
+          <p>By continuing, I agree to the terms of use & privacy policy</p>
+        </div>
+        {currentState === "Login" ? (
+          <p onClick={() => setCurrentState("Sign Up")}>
+            Create a new account? <span>Click Here</span>
+          </p>
+        ) : (
+          <p onClick={() => setCurrentState("Login")}>
+            Already have an account? <span>Login Here</span>
+          </p>
+        )}
+      </form>
+    </div>
+  );
+};
 
-export default LoginPopup
+export default LoginPopup;
